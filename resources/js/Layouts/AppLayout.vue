c173751457f77e0310649934255a2ab82e47890c
Enzo Taurines <enzodylan.taurines@gmail.com>
2021-05-11 11:53:44 +0200

    Début du projet + layout

---
+++ resources/js/Layouts/AppLayout.vue
@@ -0,0 +1,37 @@
+<template>
+    <layout>
+        <div class="container">
+            <nav class="nav">
+                <ul>
+                    <li>Home</li>
+                    <li>Search</li>
+                    <li>Account</li>
+                    <li>Actualités</li>
+                    <li>Contact</li>
+                    <li>Bibliothèque</li>
+                </ul>
+            </nav>
+        </div>
+    </layout>
+</template>
+
+<script>
+
+    export default {
+        
+    }
+</script>
+
+<style>
+    body{
+        background-color:#15202B;
+    }
+    div.container{
+        max-width:100vw;
+    }
+    nav.nav{
+        background-color:black;
+        color:white;
+        width:100px;
+    }
+</style>
